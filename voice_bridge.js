2749dc25d28dae57016a1622c0619a8a3c53a8e4
Ivan <ivanmielnic5@gmail.com>
2026-08-20 08:45:21 -0300

    version 2026-08-20-0845

---
+++ voice_bridge.js
@@ -0,0 +1,16 @@
+﻿const http = require("http");
+const { exec } = require("child_process");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/guardar") {
+    exec("powershell.exe -ExecutionPolicy Bypass -File \"C:\\Users\\ivan\\Downloads\\_Proyectos\\NaveGoLocal\\save.ps1\"", (error, stdout, stderr) => {
+      res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+      res.end(stdout || stderr || "Guardado");
+    });
+  } else {
+    res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+    res.end("NaveGo Voice Bridge activo. Usá /guardar");
+  }
+});
+
+server.listen(4000, () => console.log("Voice bridge en http://localhost:4000"));
